3969b8156d862da3bbe24af965fe519f2ad4b6b1
Даниил Стефаненков <daniilstef2391@gmail.com>
2020-07-08 16:43:46 +0300

    Добавьте файлы проекта.

---
+++ pract13_From/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace pract13_From
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(630, 444);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 463);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ pract13_From/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace pract13_From
+{
+    public partial class Form1 : Form
+    {
+        private const String PATH = "..\\..\\goods.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileStream fileStream = new FileStream(PATH, FileMode.OpenOrCreate);
+            StreamReader sR = new StreamReader(fileStream);
+            LinkedList<Good> goods = new LinkedList<Good>();
+            String str1 = String.Empty;
+            while ((str1 = sR.ReadLine()) != null && str1 != String.Empty)
+            {
+                String[] str = str1.Split('@');
+                switch (str[0])
+                {
+                    case "Пр":
+                        {
+                            goods.AddLast(new Product(str[1], Convert.ToInt32(str[2]), Convert.ToDateTime(str[3]), Convert.ToInt32(str[4])));
+                            break;
+                        }
+                    case "Па":
+                        {
+                            goods.AddLast(new Batch(str[1], Convert.ToInt32(str[2]), Convert.ToInt32(str[3]), Convert.ToDateTime(str[4]), Convert.ToInt32(str[5])));
+                            break;
+                        }
+                    case "К":
+                        {
+                            goods.AddLast(new Set(str[1], Convert.ToInt32(str[2]), str[3]));
+                            break;
+                        }
+                    default:
+                        {
+                            textBox1.AppendText("Incorrect data" + Environment.NewLine);
+                            break;
+                        }
+                }
+            }
+            sR.Close();
+            fileStream.Close();
+
+            DateTime now = DateTime.Now;
+            foreach (Good item in goods)
+            {
+                textBox1.AppendText(item.ShowInfo() + Environment.NewLine);
+                textBox1.AppendText((item.IsFresh(now) ? "Годен" : "Не годен" ) + Environment.NewLine);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+    abstract class Good
+    {
+
+        protected DateTime creationDate;
+
+        protected string name;
+
+        protected int price;
+
+        protected int expirationDate;
+
+        public virtual string ShowInfo()
+        {
+            return string.Format("Название продукта - " + name + " Цена " + price + " ");
+        }
+
+        public virtual bool IsFresh(DateTime currentDate)
+        {
+            return (currentDate < creationDate + new TimeSpan(expirationDate, 0, 0, 0));
+        }
+    }
+
+    internal class Product : Good
+    {
+
+        public Product(string name, int price, DateTime creationDate, int expirationDate)
+        {
+            base.name = name;
+            base.price = price;
+            base.creationDate = creationDate;
+            base.expirationDate = expirationDate;
+        }
+
+        public override string ShowInfo()
+        {
+            return base.ShowInfo() + string.Format("Дата производства - {0} Срок годности - {1} дней ",
+                                 creationDate, expirationDate);
+        }
+    }
+
+
+    internal class Batch : Good
+    {
+        private int amount;
+
+        public Batch(string name, int price, int amount, DateTime creationDate, int expirationDate)
+        {
+            base.name = name;
+            base.price = price;
+            base.creationDate = creationDate;
+            base.expirationDate = expirationDate;
+            this.amount = amount;
+
+        }
+
+        public override string ShowInfo()
+        {
+            return base.ShowInfo() +
+                   string.Format("\nКоличество - {0}\nДата производства - {1}\nСрок годности - {2} дней",
+                                 amount, creationDate, expirationDate);
+        }
+    }
+
+    internal class Set : Good
+    {
+        private string productSet;
+
+        public Set(string name, int price, string productSet)
+        {
+            base.name = name;
+            base.price = price;
+            this.productSet = productSet;
+
+        }
+
+        public override string ShowInfo()
+        {
+            return base.ShowInfo() +
+                   string.Format("\nПеречень продуктов - {0}",
+                                 productSet);
+        }
+
+        public override bool IsFresh(DateTime currentDate) => true;
+
+    }
+
+
